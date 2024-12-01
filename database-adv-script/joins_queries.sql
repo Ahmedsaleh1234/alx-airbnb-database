206da2bc2553f5d53278b77c9c435f3ccb9d87bb
Ahmedsaleh1234 <a12661826@gmail.com>
2024-12-01 21:46:48 +0200

    advance

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -2,11 +2,9 @@ SELECT * FROM Booking
 INNER JOIN User ON User.user_id = Booking.user_id;
 
 SELECT * FROM Property
-LEFT JOIN Review ON Property.property_id = Review.property_id;
+LEFT JOIN Review ON Property.property_id = Review.property_id
+ORDER BY user_id;
 
-SELECT * FROM 
-User LEFT JOIN Booking ON User.user_id = Booking.user_id
-UNION
-SELECT * FROM User 
-RIGHT JOIN Booking ON User.user_id = Booking.user_id
-
+SELECT * FROM User
+FULL OUTER JOIN Booking 
+ON User.user_id = Booking.user_id;
